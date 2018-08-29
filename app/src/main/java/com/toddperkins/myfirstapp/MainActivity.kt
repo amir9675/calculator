4bb956200c5686881f88add18ae94e79c0fdea25
amir9675 <me.75amir@gmail.com>
2018-08-29 05:54:47 -0700

    first

---
+++ app/src/main/java/com/toddperkins/myfirstapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.toddperkins.myfirstapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.toddperkins.myfirstapp.R.id.*
+
+class MainActivity : AppCompatActivity() {
+    label String =
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun setupcalculator() {
+        val buttons = arrayOf(button0, button1, button2, button3, button4, button5, button6, button7, button8, button9)
+        val mode =
+    }
+
+    fun didpressEqual() {
+
+    }
+
+    fun didPressClear() {
+
+    }
+
+    fun didPressNumber() {
+
+    }
+
+    fun ChangeMode() {
+
+    }
+
+    fun updatetext() {
+
+    }
+}
+
+
